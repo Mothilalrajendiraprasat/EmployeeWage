446ab45c7499725e4fe0148373b55d6d0b6e7ae3
Mothilalrajendiraprasat <98528594+Mothilalrajendiraprasat@users.noreply.github.com>
2022-01-29 08:52:10 +0530

    Update employeeWage.js

--- employeeWage.js
+++ employeeWage.js
@@ -1,25 +1,4 @@
 
-// var emp_rate_per_hr = 20;
-// var empHrs =0, empWage =0;
-// var is_full_time = 1;
-// var is_part_time =2;
-// var empcheck =  Math.floor(Math.random()*3);
-// switch(empcheck){
-//     case is_full_time:
-//     empHrs =8;
-//     break;
-    
-//     case is_part_time:
-//         empHrs = 4;
-//         break;
-         
-//     default :
-//     empHrs =0;
-
-// }
-// empWage = emp_rate_per_hr * empHrs;
-// console.log("Daily wage of employee is "+empWage);
-
 
 
 var emp_rate_per_hr = 20;
