2dc883825f2ff07833fc4e2ca515424181b20ded
Mothilalrajendiraprasat <98528594+Mothilalrajendiraprasat@users.noreply.github.com>
2022-01-29 08:36:35 +0530

    added switch statement to compute employee wage

--- employeeWage.js
+++ employeeWage.js
@@ -3,6 +3,7 @@ var emp_rate_per_hr = 20;
 var empHrs =0, empWage =0;
 var is_full_time = 1;
 var is_part_time =2;
+ // random to compute the value of random number rang of 2 number
 var empcheck =  Math.floor(Math.random()*3);
 switch(empcheck){
     case is_full_time:
@@ -17,5 +18,6 @@ switch(empcheck){
     empHrs =0;
 
 }
+ // daily wage give the employee rate per hour 20 and the emp hour 8 of day comput
 empWage = emp_rate_per_hr * empHrs;
 console.log("Daily wage of employee is "+empWage);
